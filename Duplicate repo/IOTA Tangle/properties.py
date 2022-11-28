b694515b204e80b4695a7c29184358645787e724
derrickr15 <113055700+derrickr15@users.noreply.github.com>
2022-11-27 18:28:43 -0600

    Add files via upload

---
+++ Duplicate repo/IOTA Tangle/properties.py
@@ -0,0 +1,8 @@
+# the number of times a transaction needs to be processed
+# before it is seen as valid
+RequiredProofs = 2 
+
+# the number of nodes needed to connect to when
+# adding a new node to the network for the purpose
+# of validating them when making a transaction
+numberOfValidationNodesNeeded = 2 
